f24f15cb221926a5c0a619d8d39855f368e86ab0
Aden Dawson <adendawson@c0706a-s3.ufhpc>
2025-11-09 13:33:16 -0500

    now with a docstring

--- find_nearest.py
+++ find_nearest.py
@@ -1,6 +1,22 @@
 import numpy as np
 
 def find_nearest(array, value):
+    '''
+    Find the element in an array nearest a given value
+    
+    Parameters
+    ----------
+    array: array
+        array to be searched
+    value: int, float
+        value to be searched for in the array
+
+    Returns
+    -------
+    idx: int
+        index of the array element nearest the given value
+    '''
+    
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
